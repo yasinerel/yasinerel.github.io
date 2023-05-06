afa502df4e9d3ecf448a633e67f8b2d3fd100581
Taha Yasin Erel <terel19@ku.edu.tr>
2023-05-06 13:48:59 +0300

    info done

--- app.js
+++ app.js
@@ -20,8 +20,18 @@ function handleCommand(command) {
       return "available commands: info, projects, links, clear" ;
     case "info":
       const image = '<img src="avtr.png" alt="Image of me" style="max-width: 200px;">';
-      const text = "Yo! It is Yasin. I am  a third-year computer engineering student with full scholarship at Koç University with a passion for software development. With a strong foundation in <span style='color: #f89820;'>Java, Python, C, and Assembly</span>. I have already developed several object-oriented programming projects. My skills extend beyond programming languages, with experience developing both frontend and backend applications using <span style='color: #f89820;'>React, Node.js, and Next.js.</span> Additionally,I am currently writing a mobile application with Flutter and is eager to expand their knowledge of databases. Not content to simply learn, I am actively developing a project in the field of image processing that has been accepted by Koç University's entrepreneurship institution(KWORKS). With my talent and drive, I am sure to make waves in the world of software development.";
-      return '<div style="display: flex; align-items: flex-start;"><div>' + image + '</div><div style="margin-left: 10px;">' + text + '</div></div>';
+      const text = "Yo! It is Yasin. I am a third-year computer engineering student with full scholarship at Koç University with a passion for software development. With a strong foundation in <span style='color: #f89820;'>Java, Python, C, and Assembly</span>. I have already developed several object-oriented programming projects. My skills extend beyond programming languages, with experience developing both frontend and backend applications using <span style='color: #f89820;'>React, Node.js, and Next.js.</span> Additionally,I am currently writing a mobile application with Flutter and is eager to expand their knowledge of databases. Not content to simply learn, I am actively developing a project in the field of image processing that has been accepted by Koç University's entrepreneurship institution(KWORKS). With my talent and drive, I am sure to make waves in the world of software development.";
+      const pythonIcon = '<i class="fa-brands fa-python fa-lg" style="color: #ced5de;"></i>';
+      const javaIcon = '<i class="fa-brands fa-java fa-lg" style="color: #b2bac7;"></i>';
+      const jsIcon = '<i class="fa-brands fa-square-js fa-lg" style="color: #737a87;"></i>';
+      const reactIcon = '<i class="fa-brands fa-react fa-lg" style="color: #a2acbe;"></i>';
+      const dbIcon = '<i class="fa-solid fa-database fa-lg" style="color: #b1bdd3;"></i>';
+      const linuxIcon = '<i class="fa-brands fa-linux fa-lg" style="color: #979ca5;"></i>';
+      const npmIcon = '<i class="fa-brands fa-npm fa-lg" style="color: #828a97;"></i>';
+      const gitIcon = '<i class="fa-brands fa-git-alt fa-lg" style="color: #9cabc4;"></i>';
+
+      const ig = '<img src="001-delete.png" alt="Image of me" style="max-width: 20px;">';
+      return '<div style="display: flex; align-items: flex-start;"><div>' + image + '</div><div style="margin-left: 10px;">' + text + '<br>'  + ig+ ig+ ig+ ig+ ig+ ig+ ig+ ig+ ig+ ig+ ig+ ig+ ig+ ig+ ig+ ig+ ig+ ig+ ig+ ig+ ig+ ig+ ig+ ig+ pythonIcon + " " + javaIcon + " " + jsIcon + " " + reactIcon + " " + dbIcon + " " + linuxIcon + " "+ npmIcon + " " + gitIcon;
     case "projects":
       return "Here are some projects I've worked on: Project A, Project B, Project C.";
     case "links":
